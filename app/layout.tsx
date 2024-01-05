fc60794aa069f94af76e8949cafea606351488a5
Richard Oliver Bray <richardbray89@gmail.com>
2024-01-05 15:41:05 +0000

    feat: add cv link to links page
    * fix: reapply analytics
    
    * chore: remove posthog imports
    
    * feat: added links page...
    ...created index page for components, remove according commented code
    
    * fix: issue with components not exporting properly
    
    * feat: add cv page to links

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,10 +38,12 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         </nav>
         <main className="flex min-h-screen flex-col items-center pt-20">{children}</main>
         <footer className="text-center font-light my-8">&copy; Orva Studio Limited</footer>
-        <Script id="ph-analytics">
-          {`!function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.async=!0,p.src=s.api_host+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset isFeatureEnabled onFeatureFlags getFeatureFlag getFeatureFlagPayload reloadFeatureFlags group updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures getActiveMatchingSurveys getSurveys onSessionId".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
+        {process.env.NODE_ENV !== 'development' ? (
+          <Script id="ph-analytics">
+            {`!function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.async=!0,p.src=s.api_host+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset isFeatureEnabled onFeatureFlags getFeatureFlag getFeatureFlagPayload reloadFeatureFlags group updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures getActiveMatchingSurveys getSurveys onSessionId".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
     posthog.init('phc_ntZ5DcInUnNjfuaAM9DTAmBJifOiKC6MxqrslGwkQJN',{api_host:'https://app.posthog.com'})`}
-        </Script>
+          </Script>
+        ) : null}
       </body>
     </html>
   );
--- app/links/components/PageLinks.tsx
+++ app/links/components/PageLinks.tsx
@@ -1,4 +1,5 @@
 import { ExternalLink } from 'lucide-react';
+import Link from 'next/link';
 
 export interface PageLink {
   title: JSX.Element | string;
@@ -9,17 +10,20 @@ export interface PageLink {
 export default function PageLinks({ links }: { links: PageLink[] }) {
   return (
     <section className="*:flex *:gap-2 *:text-lg *:sm:text-2xl *:mb-6 *:px-4 *:underline *:justify-center">
-      {links.map((link, index) => (
-        <a
-          href={link.href}
-          key={index}
-          className="hover:decoration-4 *:text-center"
-          {...(!link.internal && { target: '_blank', rel: 'noopener noreferrer' })}
-        >
-          <div>{link.title}</div>
-          {!link.internal && <ExternalLink />}
-        </a>
-      ))}
+      {links.map((link, index) => {
+        const Elem = link.internal ? Link : 'a';
+        return (
+          <Elem
+            href={link.href}
+            key={index}
+            className="hover:decoration-4 *:text-center"
+            {...(!link.internal && { target: '_blank', rel: 'noopener noreferrer' })}
+          >
+            <div>{link.title}</div>
+            {!link.internal && <ExternalLink />}
+          </Elem>
+        );
+      })}
     </section>
   );
 }
