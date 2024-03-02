6281c8d3e63b72620620d75909f3eee8ef737864
RichardBray <richardbray89@gmail.com>
2024-03-02 15:19:09 +0000

    config: add trailing slash to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
-const {
-  withHydrationOverlay,
-} = require("@builder.io/react-hydration-overlay/next");
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  trailingSlash: true,
   output: 'export',
   images: {
     unoptimized: true,
